35dc7cf27458ffc780da164d1ce04fd5a89093bb
JunZ03 <jzuluaga182@unab.edu.co>
2023-10-13 17:46:41 -0500

    Tarea moviles 13/10
    Signed-off-by: Lob1t0 <jdiaz514@unab.edu.co>

--- app/src/main/java/com/fabiansuarez/mywallet/Login_Activity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/Login_Activity.java
@@ -2,13 +2,23 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
 
+    private TextView tvGoRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvGoRegister =findViewById(R.id.tv_go_register);
+
+        tvGoRegister.setOnClickListener(view -> {
+            Intent goRegister = new Intent(Login_Activity.this, Register_Activity.class);
+            startActivity(goRegister);
+            finish();
+        });
     }
 }
